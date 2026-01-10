bfc66abf353afd2bbac111ba03424bd4e6bdb100
Satou20250828 <ns2024.sato@outlook.jp>
2026-01-10 16:54:26 +0000

    スケジュール管理アプリの提出（1回目）

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # トップページをスケジュール一覧ページに設定
+  root "schedules#index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # CRUD用のルートをまとめて作成
+  resources :schedules
+
+  # 確認用
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/*
+  # PWA関連
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
